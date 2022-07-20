6e486777676b6e93ae8028ac78c059e28971a33f
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-07-20 19:19:56 +0900

    [Feat] Configure main uitableview

--- UIKit_interaction/UIKit_interaction/MainViewController.swift
+++ UIKit_interaction/UIKit_interaction/MainViewController.swift
@@ -8,11 +8,25 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    private let listContents = [
+        "ScrollView Setting"
+    ]
+    
+    private let mainTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .systemBackground
         
+        configureNavBar()
+        
+        view.addSubview(mainTable)
     }
     
     private func configureNavBar() {
@@ -23,7 +37,21 @@ class MainViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
     }
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listContents.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listContents[indexPath.row]
+        return cell
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
 }
 
---
+++ UIKit_interaction/UIKit_interaction/ScrollViewController.swift
@@ -0,0 +1,10 @@
+//
+//  ScrollViewController.swift
+//  UIKit_interaction
+//
+//  Created by Hyeonsoo Kim on 2022/07/20.
+//
+
+import UIKit
+
+
